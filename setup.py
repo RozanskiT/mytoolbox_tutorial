6746a84b4b0da790049819ab181d13ac5667bf24
RozanskiT <tomaszrozanski95@gmail.com>
2025-03-06 10:57:28 +1100

    remove explicit requirements for simplicity

--- setup.py
+++ setup.py
@@ -4,14 +4,8 @@ setup(
     name='mytoolbox',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['numpy>=1.20'],
-    author='Tomasz Rozanski',
-    description='Minimal local toolbox module.',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'License :: OSI Approved :: MIT License',
+    install_requires=[
+        'numpy>=1.20',
     ],
     python_requires='>=3.8',
 )
